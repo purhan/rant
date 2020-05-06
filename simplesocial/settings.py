1ea9aa2c52a1ad760e936b431d1406f84dacb81e
Purhan <official.purhan@gmail.com>
2020-05-06 08:12:21 +0530

    added django-heroku

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,3 +133,5 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'test'
 LOGOUT_REDIRECT_URL = 'thanks'
+
+django_heroku.settings(locals())
